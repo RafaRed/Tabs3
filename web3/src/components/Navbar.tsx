790d92b8201032ebf831f18f1ce8e51fc608624a
RafaRed <rafaelszcardoso@gmail.com>
2022-08-22 17:53:20 -0300

    small fixes on navbar

--- web3/src/components/Navbar.tsx
+++ web3/src/components/Navbar.tsx
@@ -9,9 +9,9 @@ function Navbar() {
     }
 	const { activate, deactivate } = useWeb3React();
     return (
-        <div className='bg-black p-5 flex items-center justify-between select-none cursor-pointer' onClick={()=>logout(deactivate)}>
-            <p className='text-white ml-2'>TABS3</p>
-            <div className='bg-white w-4/12 sm:w-3/12 md:w-2/12 xl:w-1/12 h-7 rounded-lg flex items-center justify-center gap-1'>
+        <div className='bg-black p-5 flex items-center justify-between select-none '>
+            <p className='text-white ml-2 cursor-pointer' onClick={()=>window.location.href="/categories"}>TABS3</p>
+            <div className='bg-white w-4/12 sm:w-3/12 md:w-2/12 xl:w-1/12 h-7 rounded-lg flex items-center justify-center gap-1 cursor-pointer'  onClick={()=>logout(deactivate)}>
                 <p className='text-black'>{domain}</p>
                 <img src={imglogout}></img>
             </div>
